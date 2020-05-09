bf3ac56d759e9d07a0602f0688eb93e57a3f9d4b
IhoulineInes <ines.ihouline@gmail.com>
2020-05-09 13:00:17 +0200

    avec les unwap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::io::{self,Write};
 use std::process::{Command, Stdio};
 
@@ -12,34 +13,50 @@ fn main() -> std::io::Result<()> {
         handle.flush()? // locked
     } // STDOUT unlocked
 
+    // recuperer la commande de l'utilisateur
+
     let _stdin = io::stdin();
     let mut user_input = String::with_capacity(256);
+
     //On prend une référence mutable
-    // LECTURE sur la STDIN
     stdin.read_line(&mut user_input)?;
-    let user_input = user_input.trim_end();
+
+    if user_input.trim() == "exit" {
+        break
+    } else if user_input.trim() == "" {
+        continue
+    }
+
+    
+    let mut input_split = user_input.trim().split(" ");
+
+
+    // Executer une commande 
+
+    let cmd = input_split.next().unwrap();
+    let args = input_split;
+  
+    let mut cmd = Command::new(cmd).args(args).spawn().unwrap();
 
 
     
-    //-----------------------//
-    // Executer une commande //
-    //-----------------------//
 
 
-  
-    Command::new(user_input).status()?;
-
+     // Pipes 
     let ls = Command::new("ls")
         .arg("alh")
         .stdout(Stdio::piped())
         .spawn()
-        .expect("I was pancaked while trying to launch ls.");
+        .expect("ls ?");
 
     let process = Command::new("wc")
         .arg("--lines")
         .stdin(Stdio::from(ls.stdout.expect("Sommething wrong with ls stdin")))
         .spawn()
         .expect("whopsiel wc failled to launch");
+    
+    cmd.wait();
+    
     }
     //? sert à " propager l'erreur"
     //c'est mieux que de crash avec un unwrap ou expert
